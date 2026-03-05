5a4df0013330b6e9d5f04befbb5982117c169637
BeatrizLL83 <beatrizlopezleon83@gmail.com>
2026-03-05 14:06:49 +0100

    exercise

---
+++ challenge invoice/class.js
@@ -0,0 +1,22 @@
+class Invoice {
+    #company 
+    #address
+    #phone
+    #nif
+},
+    constructor ('name', 'address', 'phone', 'nif') {
+        this.#company = company;
+        this.#address = address;
+        this.#phone = phone;
+        this.#nif = nif;
+
+    }
+
+}
+
+
+class Customer {
+    name
+    a
+
+}
